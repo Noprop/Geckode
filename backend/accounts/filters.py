b875ed8851ae98b0dcdc1916652bdf9ef8c754f0
jeremysampl <jeremysampl@live.com>
2025-10-30 09:17:38 -0400

    forgot to remove an old filter from users

--- backend/accounts/filters.py
+++ backend/accounts/filters.py
@@ -2,25 +2,6 @@ from utils.filters import PrefixedFilterSet
 from django_filters import OrderingFilter
 from .models import User
 
-from rest_framework.filters import BaseFilterBackend
-from .serializers import UserSearchSerializer
-from django.db.models import Q
-
-class UserSearchFilterBackend(BaseFilterBackend):
-    def filter_queryset(self, request, queryset, view):
-        serializer = UserSearchSerializer(data=request.query_params)
-        serializer.is_valid(raise_exception=True)
-        params = serializer.validated_data
-
-        if 'search' in params:
-            queryset = queryset.filter(
-                Q(username__icontains=params['search']) |
-                Q(first_name__icontains=params['search']) |
-                Q(last_name__icontains=params['search'])
-            )
-
-        return queryset.order_by(params['order_by'])
-
 class UserFilter(PrefixedFilterSet):
     search_fields = User.SEARCH_FIELDS
 
