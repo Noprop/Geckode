17d157c785e0ccc1c1718ab4204dc433dcfeb82c
Noprop <12greg21@gmail.com>
2025-11-13 14:03:54 -0500

    phaser canvas is now a static resolution

--- frontend/src/components/PhaserGame.tsx
+++ frontend/src/components/PhaserGame.tsx
@@ -1,16 +1,15 @@
 "use client";
 
 import { useEffect, useLayoutEffect, useRef, useImperativeHandle } from 'react';
-import StartGame from '@/phaser/main';
+import StartGame from '@/phaser/phaserConfig';
 import { EventBus } from '@/phaser/EventBus';
 import { MAIN_MENU_SCENE_KEY } from '@/phaser/scenes/MainMenu';
 import type MainMenu from '@/phaser/scenes/MainMenu';
-
-type PhaserInstance = ReturnType<typeof StartGame>;
+import type { Game } from 'phaser';
 
 // the React hooks in this component are written in order of their actual execution.
 const PhaserGame = ({ ref }: any) => {
-  const gameRef = useRef<PhaserInstance | null>(null);
+  const gameRef = useRef<Game | null>(null);
 
   useLayoutEffect(() => {
     if (!gameRef.current) gameRef.current = StartGame('game-container');
@@ -29,11 +28,9 @@ const PhaserGame = ({ ref }: any) => {
   );
 
   useEffect(() => {
-    // this scene event listener will live until this
-    // Phaser react component has been torn down
+    // this scene event listener will live until this Phaser react component has been torn down
     const handler = (scene: Phaser.Scene | MainMenu) => {
       if (!('key' in scene) || !(scene.key == MAIN_MENU_SCENE_KEY)) return;
-      ref.scene = scene;
       ref.current.scene = scene;
     };
     EventBus.on('current-scene-ready', handler);
@@ -43,7 +40,9 @@ const PhaserGame = ({ ref }: any) => {
     };
   }, []);
 
-  return <div id="game-container" className="h-96" />;
+  return (
+    <div id="game-container" style={{ height: '360px', width: '480px' }} />
+  );
 };
 
 export default PhaserGame;
--- frontend/src/phaser/phaserConfig.ts
+++ frontend/src/phaser/phaserConfig.ts
@@ -8,15 +8,12 @@ import { Preloader } from './scenes/Preloader';
 // Find out more information about the Game Config at:
 // https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 
-let gameViewWidth : number = window.innerWidth * 0.4;
-let gameViewHeight : number = gameViewWidth * 9 / 16
-
 const config = {
   type: Phaser.AUTO,
-  width: gameViewWidth,
-  height: gameViewHeight,
-  scale: {mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT},
-  parent: 'game-container',
+  width: 480,
+  height: 360,
+  scale: { mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT },
+  parent: 'phaser-container',
   backgroundColor: '#707090',
   scene: [Boot, Preloader, MainMenu, Game, GameOver],
   physics: {
@@ -29,7 +26,6 @@ const config = {
 } satisfies Phaser.Types.Core.GameConfig;
 
 const StartGame = (parent: string | HTMLElement): Phaser.Game => {
-  console.log('parent: ', parent);
   return new Phaser.Game({ ...config, parent });
 };
 
