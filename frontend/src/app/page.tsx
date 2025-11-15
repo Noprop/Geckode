466bccef353e7ca3a1504bfedcc3c0854dac5e57
jeremysampl <jeremysampl@live.com>
2025-11-15 15:57:25 -0500

    allow option to load blank workspace on /

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,3 @@
-"use client";
+import ProjectView from '@/components/ProjectView';
 
-export default function Home() {
-  return (
-    <div className="flex h-[calc(100vh-4rem)]">
-      <h2
-        className="text-center w-full underline cursor-pointer m-40 text-blue-600"
-        onClick={() => (window.location.href = "/projects/")}
-      >
-        Please go to /projects/ to pick out a project to edit interface
-      </h2>
-    </div>
-  );
-}
+export default ProjectView;
