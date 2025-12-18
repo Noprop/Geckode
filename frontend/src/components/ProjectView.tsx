c13883c8379558e994ab6a035b31f35284b0b144
Noprop <12greg21@gmail.com>
2025-12-18 09:45:56 -0500

    update the undo/redo button icons

--- frontend/src/components/ProjectView.tsx
+++ frontend/src/components/ProjectView.tsx
@@ -575,12 +575,26 @@ const ProjectView: React.FC<ProjectViewProps> = ({ projectId }) => {
             Export Workspace
           </Button>
 
+          {/* The ResetIcon is also viable. IMO, the svg looks better now as of Dec 18, 2025 */}
           <Button onClick={undoWorkspace} className="btn-neutral" title="Undo">
-            <ResetIcon
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              className="octicon octicon-undo"
+              viewBox="0 0 16 16"
+              width="16"
+              height="16"
+              fill="currentColor"
+              display="inline-block"
+              overflow="visible"
+            >
+              <path d="M1.22 6.28a.749.749 0 0 1 0-1.06l3.5-3.5a.749.749 0 1 1 1.06 1.06L3.561 5h7.188l.001.007L10.749 5c.058 0 .116.007.171.019A4.501 4.501 0 0 1 10.5 14H8.796a.75.75 0 0 1 0-1.5H10.5a3 3 0 1 0 0-6H3.561L5.78 8.72a.749.749 0 1 1-1.06 1.06l-3.5-3.5Z"></path>
+            </svg>
+            {/* <ResetIcon
               width={20}
               height={20}
               style={{ verticalAlign: 'middle' }}
-            />
+            /> */}
           </Button>
           <Button onClick={redoWorkspace} className="btn-neutral" title="Redo">
             <span
@@ -590,11 +604,25 @@ const ProjectView: React.FC<ProjectViewProps> = ({ projectId }) => {
                 verticalAlign: 'middle',
               }}
             >
-              <ResetIcon
+              {/* <ResetIcon
                 width={20}
                 height={20}
                 style={{ verticalAlign: 'middle' }}
-              />
+              /> */}
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                className="octicon octicon-undo"
+                viewBox="0 0 16 16"
+                width="16"
+                height="16"
+                fill="currentColor"
+                display="inline-block"
+                overflow="visible"
+                style={{ verticalAlign: 'middle' }}
+              >
+                <path d="M1.22 6.28a.749.749 0 0 1 0-1.06l3.5-3.5a.749.749 0 1 1 1.06 1.06L3.561 5h7.188l.001.007L10.749 5c.058 0 .116.007.171.019A4.501 4.501 0 0 1 10.5 14H8.796a.75.75 0 0 1 0-1.5H10.5a3 3 0 1 0 0-6H3.561L5.78 8.72a.749.749 0 1 1-1.06 1.06l-3.5-3.5Z"></path>
+              </svg>
             </span>
           </Button>
         </div>
