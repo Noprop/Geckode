582e7372d3881b889e9347ec3695cb33031b6e01
diceccoj <107959943+diceccoj@users.noreply.github.com>
2026-02-22 22:23:56 -0500

    More bug fixes

--- frontend/src/app/(default)/layout.tsx
+++ frontend/src/app/(default)/layout.tsx
@@ -1,18 +1,11 @@
-import Header from "@/components/Header/Header";
-import HeaderRHSBtns from "@/components/HeaderRHSBtns";
+import Header from '@/components/Header/Header';
+import HeaderRHSBtns from '@/components/Header/HeaderRHSBtns';
 
-export default function EditorLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function EditorLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header rhs={<HeaderRHSBtns />} />
-      <main
-        className="flex-1 flex flex-col bg-light-secondary dark:bg-dark-secondary"
-        id="app"
-      >
+      <main className='flex-1 flex flex-col bg-light-secondary dark:bg-dark-secondary' id='app'>
         {children}
       </main>
     </>
