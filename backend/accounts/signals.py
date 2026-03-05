cf229279947c599801e81d4ec5c717ad57c1d267
jeremysampl <jeremysampl@live.com>
2026-03-05 10:59:34 -0500

    Fixed old user avatars not deleting

---
+++ backend/accounts/signals.py
@@ -0,0 +1,34 @@
+from django.db.models.signals import pre_save, post_delete
+from django.dispatch import receiver
+from .models import User
+
+@receiver(pre_save, sender=User)
+def delete_old_avatar_on_change(sender, instance: User, **kwargs) -> None:
+    """
+    Delete the old avatar file from storage when a new one is uploaded.
+    """
+    if not instance.pk:
+        # New user, no previous avatar to delete
+        return
+
+    try:
+        old_user = sender.objects.get(pk=instance.pk)
+    except sender.DoesNotExist:
+        return
+
+    old_avatar = old_user.avatar
+    new_avatar = instance.avatar
+
+    # If avatar changed and old file exists, delete the old file
+    if old_avatar and old_avatar != new_avatar:
+        old_avatar.delete(save=False)
+
+
+@receiver(post_delete, sender=User)
+def delete_avatar_on_user_delete(sender, instance: User, **kwargs) -> None:
+    """
+    Delete avatar file from storage when the user is deleted.
+    """
+    avatar = instance.avatar
+    if avatar:
+        avatar.delete(save=False)
